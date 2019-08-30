bcf755a0bc1de03ece1bf64f6154d4557c2325a5
Kristin Lucas <klucas11@aggienetwork.com>
2019-08-30 16:22:06 -0500

    buildMetaData function

--- static/js/app.js
+++ static/js/app.js
@@ -5,15 +5,20 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-
+  var metadataURL = `/metadata/${sample}`;
   // Use d3 to select the panel with id of `#sample-metadata`
-
-  // Use `.html("") to clear any existing metadata
-
-  // Use `Object.entries` to add each key and value pair to the panel
-  // Hint: Inside the loop, you will need to use d3 to append new
-  // tags for each key-value in the metadata.
-
+  d3.json(metadataURL).then(function (sample) {
+    var sampleData = d3.select(`#sample-metadata`);
+    // Use `.html("") to clear any existing metadata
+    sampleData.html("");
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(sample).forEach(function ([key, value]) {
+      var row = sampleData.append("p");
+      row.text(`${key}:${value}`)
+    })
+  });
 
   // BONUS: Build the Gauge Chart
   // buildGauge(data.WFREQ);
